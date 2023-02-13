1fa79093481e3e0a37b72901abf80137310b3f94
SN <sn.sarvinm@gmail.com>
2023-02-13 11:29:40 +0330

    edited line charted

--- src/app/features/daily-price/components/line-chart-daily-price/line-chart-daily-price.component.ts
+++ src/app/features/daily-price/components/line-chart-daily-price/line-chart-daily-price.component.ts
@@ -54,9 +54,10 @@ export class LineChartDailyPriceComponent implements OnInit , AfterViewInit, OnC
         //   month: '%e. %b',
         //   year: '%b'
         // }
+        reversed: true,
         categories: this.namechart,
         labels: {
-          rotation: 30,
+          rotation: 10,
         }
 
       },
@@ -74,8 +75,97 @@ export class LineChartDailyPriceComponent implements OnInit , AfterViewInit, OnC
   }
 
   ngOnChanges(){
-
+    // @ts-ignore
+    // Highcharts.chart('container', {
+    //
+    //   title: {
+    //     text: 'U.S Solar Employment Growth by Job Category, 2010-2020',
+    //     align: 'left'
+    //   },
+    //
+    //   subtitle: {
+    //     text: 'Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>',
+    //     align: 'left'
+    //   },
+    //
+    //   yAxis: {
+    //     title: {
+    //       text: 'Number of Employees'
+    //     }
+    //   },
+    //
+    //   xAxis: {
+    //     // reversed: true,
+    //     // categories: this.namechart,
+    //     // labels: {
+    //     //   rotation: 10,
+    //     // },      // reversed: true,
+    //     categories: this.namechart,
+    //     // labels: {
+    //     //   rotation: 10,
+    //     // },
+    //
+    //     accessibility: {
+    //       rangeDescription:  this.namechart
+    //     }
+    //   },
+    //
+    //   legend: {
+    //     layout: 'vertical',
+    //     align: 'right',
+    //     verticalAlign: 'middle'
+    //   },
+    //
+    //   plotOptions: {
+    //     series: {
+    //       label: {
+    //         connectorAllowed: false
+    //       },
+    //       pointStart: 2010
+    //     }
+    //   },
+    //
+    //   series: [{
+    //     name: 'Installation & Developers',
+    //     data: this.dataChart
+    //   }
+    //   // , {
+    //   //   name: 'Manufacturing',
+    //   //   data: [24916, 37941, 29742, 29851, 32490, 30282,
+    //   //     38121, 36885, 33726, 34243, 31050]
+    //   // }, {
+    //   //   name: 'Sales & Distribution',
+    //   //   data: [11744, 30000, 16005, 19771, 20185, 24377,
+    //   //     32147, 30912, 29243, 29213, 25663]
+    //   // }, {
+    //   //   name: 'Operations & Maintenance',
+    //   //   data: [null, null, null, null, null, null, null,
+    //   //     null, 11164, 11218, 10077]
+    //   // }, {
+    //   //   name: 'Other',
+    //   //   data: [21908, 5548, 8105, 11248, 8989, 11816, 18274,
+    //   //     17300, 13053, 11906, 10073]
+    //   // }
+    //   ],
+    //
+    //   responsive: {
+    //     rules: [{
+    //       condition: {
+    //         maxWidth: 500
+    //       },
+    //       chartOptions: {
+    //         legend: {
+    //           layout: 'horizontal',
+    //           align: 'center',
+    //           verticalAlign: 'bottom'
+    //         }
+    //       }
+    //     }]
+    //   }
+    //
+    // });
   }
+
 
 
 
