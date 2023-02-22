5ade1006c22b318f4c42d05a9b2736bd079cae5f
SN <sn.sarvinm@gmail.com>
2023-02-22 11:46:32 +0330

    added responsive error in auth

--- src/app/core/interceptor/auth.interceptor.ts
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -3,27 +3,87 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, tap} from 'rxjs';
 import {AuthService} from "../services";
+import {Router} from "@angular/router";
 
 @Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+              private router: Router) {}
+
+  //
+  // intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  //
+  //   request = request.clone({
+  //     setHeaders: {
+  //       'Content-Type': 'application/json; charset=utf-8',
+  //       'Accept': ' text/plain',
+  //       'Authorization': `Bearer ${this.authService.getAuthToken()}`,
+  //     }
+  //   })
+  //
+  //   return next.handle(request);
+  // }
+
+
+  // intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  //   // @ts-ignore
+  //   const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  //   if (currentUser && currentUser.token) {
+  //     request = request.clone({
+  //       setHeaders: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: `JWT ${currentUser.token}`
+  //       }
+  //     });
+  //   }
+  //
+  //
+  //   return next.handle(request).pipe( tap(() => {},
+  //     (err: any) => {
+  //       if (err instanceof HttpErrorResponse) {
+  //         if (err.status !== 401 ) {
+  //           return;
+  //         }
+  //         this.router.navigate(['./auth/login']);
+  //       }
+  //     }));
+  // }
+
+
+
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     request = request.clone({
       setHeaders: {
-        'Content-Type' : 'application/json; charset=utf-8',
-        'Accept'       : ' text/plain',
+        'Content-Type': 'application/json; charset=utf-8',
+        'Accept': ' text/plain',
         'Authorization': `Bearer ${this.authService.getAuthToken()}`,
       }
     })
 
-    return next.handle(request);
+      return next.handle(request).pipe( tap(() => {},
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status !== 401 ) {
+              return;
+            }
+            this.router.navigate(['./auth/login']);
+          }
+        }));
   }
+
+
+
+
+
+
+
+
 }
